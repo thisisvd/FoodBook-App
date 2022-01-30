5f9fa68ff3b98524d09dda0cd1ac751f1f5f9a14
Vimal Dubey <vimaldubey123@outlook.com>
2022-01-31 01:00:41 +0530

    update ...

--- app/src/main/java/com/vdcodeassociate/foodbook/MainActivity.kt
+++ app/src/main/java/com/vdcodeassociate/foodbook/MainActivity.kt
@@ -2,10 +2,50 @@ package com.vdcodeassociate.foodbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.vdcodeassociate.foodbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // viewBinding
+    private lateinit var binding: ActivityMainBinding
+
+    // nav Controller
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set up nav host fragment with nav controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // configure app bar
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favouriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        // set up bottom up navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // set up app bar with navController
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
